0c1a58b93484d8ac5f04a1acb9a9b1baa670ff10
Sam Zimmerman <smz7804@rit.edu>
2017-05-15 11:35:53 -0400

    Finished up P2

--- P2.py
+++ P2.py
@@ -1,3 +1,8 @@
+"""
+Problem 2
+Lorenz Attractor and Correlation Dimension
+"""
+
 import matplotlib.pyplot as plt
 import numpy as np
 from tqdm import tqdm
@@ -11,10 +16,10 @@ def Lorenz(X, r=1., sigma=10., b=8./3.):
     return np.array([Xdot, Ydot, Zdot])
     
 def LLFit(x, m, b):
-    return np.exp(np.log(x)*m + b)
+    return m*x + b
 
 x0 = [3., 0., 0.]
-r = 100
+r = 28
 alpha = 0.01
 
 x = np.array(x0)
@@ -27,24 +32,31 @@ for j in tqdm(xrange(50000)):
     k3 = Lorenz(x + k2*alpha/2., r)
     k4 = Lorenz(x + k3*alpha, r)
     x += alpha*(k1 + 2*k2 + 2*k3 + k4)/6
-    if j > 10000:
+    if j > 40000:
         XL.append(x+0)
-
+'''
 XX, YY, ZZ = zip(*XL)
-#plt.plot(XX, YY, 'b.')
-#plt.close()
+plt.plot(XX, YY, 'b.')
+plt.grid()
+plt.close()
+'''
 
 cts = []
 Count = 0
-rs = np.logspace(-3,1,10)
+rs = np.logspace(-3,2,50)
 for r in tqdm(rs):
-    for i in tqdm(xrange(100)):
+    for i in tqdm(xrange(10)):
         cx, cy, cz = XL[np.random.choice(len(XL))]
         Count += sum([(x-cx)**2 + (y-cy)**2 + (z-cz)**2 < r**2 for x, y, z in XL])
-    cts.append(Count/100)
+    cts.append(Count/100.)
 
-CF, _ = curve_fit(LLFit, rs, cts)
+CF, _ = curve_fit(LLFit, np.log(rs[30:-7]), np.log(cts[30:-7]))
 
 plt.loglog(rs, cts, 'bx')
-plt.loglog(rs, LLFit(rs, *CF), 'r--')
-plt.show()
+plt.loglog(rs, np.exp(LLFit(np.log(rs), *CF)), 'r--')
+plt.title("Fit Eqn: {}x + {}".format(CF[0], CF[1]))
+plt.grid()
+plt.xlabel('Radius of aperture')
+plt.ylabel('Avg number of neighbors')
+plt.savefig('P22.png', format='png', dpi=300)
+plt.close()
