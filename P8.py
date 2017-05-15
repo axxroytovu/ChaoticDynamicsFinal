772cb771e4aae121226d3258cafa3961ff77514f
Sam Zimmerman <smz7804@rit.edu>
2017-05-15 15:51:38 -0400

    Completely redid Problem 8. Now it’s much slicker and actually works.

--- P8.py
+++ P8.py
@@ -8,94 +8,60 @@ import numpy as np
 from tqdm import tqdm
 import itertools as itr
 
+def CSand(Sand, CRITVAL):
+    SandX = Sand[1:, :] - Sand[:-1, :]
+    SandY = Sand[:, 1:] - Sand[:, :-1]
+    MaxTotalX = np.unravel_index(np.argmax(np.abs(SandX)), SandX.shape)
+    MaxTotalY = np.unravel_index(np.argmax(np.abs(SandY)), SandY.shape)
+    if abs(SandX[MaxTotalX]) >= abs(SandY[MaxTotalY]):
+        MaxTotCoord = MaxTotalX
+        Dir = 0
+    else:
+        MaxTotCoord = MaxTotalY
+        Dir = 1
+    Value = [SandX, SandY][Dir][MaxTotCoord]
+    PosNeg = np.sign(Value) == 1
+    #print MaxTotCoord, Dir, PosNeg, Value
+    #print Sand
+    #raw_input()
+    return abs(Value)>CRITVAL, [abs(Value),list(MaxTotCoord),Dir,PosNeg]
+
 Asize = []
-CRITVAL = 7
+CritStatic = 7
+CritDynamic = 6
 
 for i in tqdm(xrange(10000)):
     Sand = np.zeros((10,10))
     check = True
+    size = 0
     while True:
         i, j = np.random.randint(10, size=2)
         Sand[i, j] += 1
-        if i != 0:
-            xm = abs(Sand[i, j] - Sand[i-1, j])
-            if xm > CRITVAL:
-                check = False
+        Av, [V, C, D, P] = CSand(Sand, CritStatic)
+        while Av:
+            size += V/2
+            #print V, C, D, P
+            C[D] += P
+            #print C
+            P = P*2 - 1
+            #print P
+            Sand[C[0], C[1]] -= V/2
+            #print Sand
+            C[D] -= P
+            #print C
+            Sand[C[0], C[1]] += V/2
+            #print Sand
+            #raw_input()
+            Av, [V, C, D, P] = CSand(Sand, CritDynamic)
+            if not Av:
+                break
         else:
-            xm = 0
-        if j != 0:
-            ym = abs(Sand[i, j] - Sand[i, j-1])
-            if ym > CRITVAL:
-                check = False
-        else:
-            ym = 0
-        if j != 9:
-            yp = abs(Sand[i, j] - Sand[i, j+1])
-            if yp > CRITVAL:
-                check = False
-        else:
-            yp = 0
-        if i != 9:
-            xp = abs(Sand[i, j] - Sand[i+1, j])
-            if xp > CRITVAL:
-                check = False
-        else:
-            xp = 0
-        size = 0
-        while not check:
-            mx = max([xm, xp, ym, yp])
-            print mx, xm, xp, ym, yp
-            raw_input()
-            if mx == xm:
-                Sand[i, j] -= int(mx)/2
-                Sand[i-1, j] += int(mx)/2
-                i -= 1
-                size += int(mx)/2
-            if mx == xp:
-                Sand[i, j] -= int(mx)/2
-                Sand[i+1, j] += int(mx)/2
-                i += 1
-                size += int(mx)/2
-            if mx == yp:
-                Sand[i, j] -= int(mx)/2
-                Sand[i, j+1] += int(mx)/2
-                j += 1
-                size += int(mx)/2
-            if mx == ym:
-                Sand[i, j] -= int(mx)/2
-                Sand[i, j-1] += int(mx)/2
-                j -= 1
-                size += int(mx)/2
-            check = True
-            print i, j
-            raw_input()
-            #for i, j in itr.product(xrange(10), xrange(10)):
-            if i != 0:
-                xm = (Sand[i, j] - Sand[i-1, j])
-                if xm > CRITVAL:
-                    check = False
-            else:
-                xm = 0
-            if j != 0:
-                ym = (Sand[i, j] - Sand[i, j-1])
-                if ym > CRITVAL:
-                    check = False
-            else:
-                ym = 0
-            if j != 9:
-                yp = (Sand[i, j] - Sand[i, j+1])
-                if yp > CRITVAL:
-                    check = False
-            else:
-                yp = 0
-            if i != 9:
-                xp = (Sand[i, j] - Sand[i+1, j])
-                if xp > CRITVAL:
-                    check = False
-            else:
-                xp = 0
-        if size:
-            Asize.append(size)
-            break
+            continue
+        Asize.append(size)
+        break
 plt.hist(Asize)
+plt.grid()
+plt.xlabel('Avalanche Size')
+plt.ylabel('Number of Occurences')
+plt.title('Model of Avalanche Occurrence')
 plt.show()
