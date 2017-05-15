197c142cde1c0d861e5f8a808b4620a63d18fcab
Sam Zimmerman <smz7804@rit.edu>
2017-05-15 15:51:15 -0400

    Finished up Problem 7

--- P7.py
+++ P7.py
@@ -7,9 +7,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 
-#xmin, xmax, ymin, ymax = -2, 1, -1.5, 1.5
+xmin, xmax, ymin, ymax = -2, 1, -1.5, 1.5
 #xmin, xmax, ymin, ymax = -1.3, -0.8, -0.6, -0.1
-xmin, xmax, ymin, ymax = -1, -0.9, -0.35, -0.25
+#xmin, xmax, ymin, ymax = -1, -0.9, -0.35, -0.25
 
 xx, yy = np.meshgrid(np.linspace(xmin, xmax, 2000), np.linspace(ymin, ymax, 2000))
 complex = xx + yy*1.0j
@@ -25,5 +25,6 @@ for i in tqdm(range(100)):
     complex[np.abs(complex) > 10] = 0
     
 plt.imshow(show, extent=[xmin, xmax, ymin, ymax], origin='lower', cmap="nipy_spectral_r", clim=[0,100])
-plt.show()
+plt.savefig('P7.png', format='png', dpi=300)
+plt.close()
     
